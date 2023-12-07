19bf1349644e87a5e27d9d56c9a94f4ac05ec961
Codewithsonu <sesettijaswanth@gmail.com>
2023-12-07 13:40:25 +0000

    " Few Changes are added "

--- webapp/controller/Mainpage.controller.js
+++ webapp/controller/Mainpage.controller.js
@@ -1,21 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    'sap/ui/core/Fragment'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,Fragment) {
         "use strict";
 
         return Controller.extend("com.sap.teammarvelhub.controller.Mainpage", {
             onInit: function () {
+
+                
 
             },
             
             // when we click on the notification button these function will trigger 
             onPressNotification:function(){
 
-                
+
 
             }
         });
